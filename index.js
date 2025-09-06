70007f8b131c0972764f43120fd91152ef4eab21
omrylmz <omeryilmaz991@gmail.com>
2025-09-06 14:57:56 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import { registerRootComponent } from 'expo';
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures the app works in Expo Go and in a standalone build.
+registerRootComponent(App);
